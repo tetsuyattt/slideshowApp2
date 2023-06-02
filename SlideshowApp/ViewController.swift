e2400760f542a18271bb9f54cf46c9f044edc216
t.tetsuya <tetuyatt522@gmail.com>
2023-06-02 23:10:54 +0900

    再生停止ボタン押したら、再生して他のボタン使えなくできたしテキストも再生中にできた、同じボタン押したら停止できたし他のボタンを使えるようにできた

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -11,12 +11,27 @@ class ViewController: UIViewController {
     
     let imageName = ["flower1", "flower2", "flower3"]
     var changeImgNo = 0
+   
+    var timer: Timer! //タイマー
     
     
-    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
     
 
-    @IBAction func gobtn(_ sender: Any) {
+    func displayImage() {
+        let imageName = ["flower1", "flower2", "flower3"]
+        //上流のプロパティで「let imageName = ["flower1", "flower2", "flower3"]」宣言しているので改めて宣言しなくてok？
+        if changeImgNo < 0 {changeImgNo = 2 }
+        if changeImgNo > 2 {changeImgNo = 0 }
+        //表示中の画像Noから名前を取り出す　nameが”　”になる
+        let name = imageName[changeImgNo]
+        imageView.image = UIImage(named: name)
+    }
+
+    
+    @IBAction func nextbtn(_ sender: Any) {
         if changeImgNo == 0 {
             changeImgNo = 1
         } else if changeImgNo == 1 {
@@ -24,7 +39,47 @@ class ViewController: UIViewController {
         else if changeImgNo == 2 {
             changeImgNo = 0 }
         let name = imageName[changeImgNo]
-        image.image = UIImage(named: name) }
+        imageView.image = UIImage(named: name) //最後のこの２行は必須っぽい
+    }
+    
+ 
+    @IBOutlet weak var nextbtn: UIButton!  //押せなくなるのに必須
+    @IBOutlet weak var backbtn: UIButton!
+    
+    
+    @IBAction func startstopbtn(_ sender: UIButton){
+       
+        if self.timer == nil {
+            timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(timerUpdate), userInfo: nil, repeats: true)
+            nextbtn.isEnabled = false//再生停止ボタン押すと、他のボタンが押せなくなる
+            backbtn.isEnabled = false
+            sender.setTitle("再生中", for: .normal)
+        }
+        
+        else {      //タイマーが"nil"じゃない時
+            self.timer.invalidate() //タイマー止める
+            self.timer = nil
+            nextbtn.isEnabled = true  //他のボタン復活
+            backbtn.isEnabled = true
+            sender.setTitle("停止中", for: .normal)
+        }
+        
+    }
+        
+        // let name = imageName[changeImgNo],imageView.image = UIImage(named: name)←ここにあったけど、なくていい
+    
+    
+    @objc private func timerUpdate() {
+            print("onTimer")
+            changeImgNo += 1
+            displayImage()
+    }
+        
+    private func display() {
+        let name = imageName[changeImgNo]
+        imageView.image = UIImage(named: name)
+    }   //←ChatGPTに教えてもらったらやつ、自動送りされるようになった
+    
     
     @IBAction func backbtn(_ sender: Any) {
         if changeImgNo == 0 {
@@ -34,7 +89,8 @@ class ViewController: UIViewController {
         else if changeImgNo == 1 {
             changeImgNo = 0 }
         let name = imageName[changeImgNo]
-        image.image = UIImage(named: name)
+        imageView.image = UIImage(named: name)
+    //startstopボタンのtimer以下はここにあった
     }
     
     override func viewDidLoad() {
@@ -42,9 +98,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         let imageName = UIImage(named: "flower1")
-        image.image = imageName
-        
+        imageView.image = imageName
     }
     
+ 
 }
 
